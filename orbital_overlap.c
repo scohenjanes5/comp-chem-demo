4e0e65093471797e8343b18a248f6a6ef3a22289
scohenjanes5 <101665992+scohenjanes5@users.noreply.github.com>
2022-07-08 11:48:13 -0400

    Read basis set and assign values for Hydrogen.
    still need to assign values for Oxygen

--- orbital_overlap.c
+++ orbital_overlap.c
@@ -7,7 +7,7 @@
 
 //store orbital info
 struct Orbital {
-    double primC, expC;
+    double primC[num_dimensions], expC[num_dimensions];
     int angular_momentum_vector[num_dimensions];
     double center[num_dimensions];
     int parent_atom_Z, parent_atom_idx;
@@ -15,13 +15,20 @@ struct Orbital {
 
 void orbital_info(struct Orbital orb, int idx);
 void get_geom_details(struct Orbital orbital_array[BS_Size], FILE *geom_pointer);
+void get_coefs(struct Orbital orbital_array[BS_Size], FILE *coef_pointer);
 
 int main(){
     //get info from files.
     //geometry
-    FILE *geom_pointer;
+    FILE *geom_pointer, *coef_pointer;
     geom_pointer = fopen("water_wolfram.xyz", "r");
+    coef_pointer = fopen("cont_exp_coefs.csv", "r");
     get_geom_details(orbital_array, geom_pointer);
+    get_coefs(orbital_array, coef_pointer);
+    // check out all orbitals in detail
+    for (int i = 0; i < Num_Orbitals; i++){
+        orbital_info(orbital_array[i], i);
+    }
 
     // check to see angular momentum vectors are correct.
     // for (int i = 0; i < BS_Size; i++){
@@ -46,14 +53,12 @@ void get_geom_details(struct Orbital orbital_array[BS_Size], FILE *geom_pointer)
     int additional_orbitals;
     // Reading coords and atomic number from file
     while (!feof(geom_pointer)){
-        printf("atom number %d\n", atom_idx);
         fscanf(geom_pointer, "%d %lf %lf %lf", 
             &orbital_array[orbital_idx].parent_atom_Z, &orbital_array[orbital_idx].center[0], 
             &orbital_array[orbital_idx].center[1], &orbital_array[orbital_idx].center[2]
         );
 
-        printf("atom %d has atomic number %d\n",atom_idx, orbital_array[orbital_idx].parent_atom_Z);
-        
+        // printf("atom %d has atomic number %d\n",atom_idx, orbital_array[orbital_idx].parent_atom_Z);        
         if(orbital_array[orbital_idx].parent_atom_Z > 2){
             //4 more orbitals for n=2
             additional_orbitals = 4;
@@ -82,13 +87,40 @@ void get_geom_details(struct Orbital orbital_array[BS_Size], FILE *geom_pointer)
         orbital_idx += additional_orbitals+1;
         atom_idx++;
     }
-    printf("\n");
+}
 
-    // check out all orbitals in detail
-    for (int i = 0; i < Num_Orbitals; i++){
-        orbital_info(orbital_array[i], i);
+void get_coefs(struct Orbital orbital_array[BS_Size], FILE *coef_pointer){
+    if (NULL == coef_pointer){
+        printf("file can't be opened\n");
     }
 
+    double basis_array[BS_Size * num_dimensions * 2]; //size is total num of coeffs.
+
+    int BS_idx = 0;
+    while (!feof(coef_pointer)){
+        fscanf(coef_pointer, "%lf %lf %lf %lf %lf %lf", 
+            &basis_array[BS_idx], &basis_array[BS_idx+1], &basis_array[BS_idx+2], //contraction coefs
+            &basis_array[BS_idx+3], &basis_array[BS_idx+4], &basis_array[BS_idx+5] //exponential coefs
+        );
+        BS_idx += 6; //go to next block of empty entries.
+    }
+    //the basis is defined as H1s H2s O1s O2s O2p... so we can just use direct assignment.
+    for (int i = 0; i < Num_Orbitals; i++){
+        if (orbital_array[i].parent_atom_Z == 1 && i % 2 == 0){
+            //we can't yet distinguish between 1s and 2s so we should assign coefs to both now.
+            for(int j = 0; j < num_dimensions; j++){
+                orbital_array[i].primC[j] = basis_array[j]; //assign 1s orbital coefs here
+                orbital_array[i].expC[j] = basis_array[num_dimensions + j]; // ^ and here.
+                orbital_array[i+1].primC[j] = basis_array[j + num_dimensions * 2]; //now assign 2s coefs here
+                orbital_array[i+1].expC[j] = basis_array[j + num_dimensions * 2 + num_dimensions]; //and here too.
+            }
+            printf("done with a hydrogen atom's orbitals\n");
+            continue;
+        }
+        if (orbital_array[i].parent_atom_Z == 8){
+            printf("this orbital is attached to oxygen\n");
+        }
+    }
 }
 
 void orbital_info(struct Orbital orb, int idx){
@@ -97,11 +129,17 @@ void orbital_info(struct Orbital orb, int idx){
         printf(" %d", orb.angular_momentum_vector[j]);
     }
     printf(" }\n");
+    char dimensions[] = "xyz";
+    printf("It has the coefficients\ncontraction     exponential\n");
+    for (int j = 0; j < num_dimensions; j++){
+        printf("%c: %lf     %lf\n", dimensions[j], orb.primC[j],orb.expC[j]);
+    }
+    printf("\n");
     printf("it is located at coordinates: (");
     for (int j = 0; j < num_dimensions; j++){
         printf(" %lf",orb.center[j]);
 
     }
     printf(" )\n");
-    printf("it is on atom #%d, which has atomic number %d\n", orb.parent_atom_idx, orb.parent_atom_Z);
+    printf("it is on atom #%d, which has atomic number %d\n\n----------------\n", orb.parent_atom_idx, orb.parent_atom_Z);
 }
