8ef48282404c5df7a803e2ef70cde9298f2c2dba
scohenjanes5 <101665992+scohenjanes5@users.noreply.github.com>
2022-08-03 14:48:03 -0400

    change for to while loop

--- alternate_boys_evaluation.c
+++ alternate_boys_evaluation.c
@@ -14,13 +14,12 @@ int main(){
 double hyp1f1_clone(double a, double b, double x){
     double term = 1.0;
     double result = 1.0;
-    for (int k = 0; k < 500; k++){
+    int k = 0;
+    while (fabs(term) > EPS * fabs(result)){
         //since each term is almost what is needed for the next, why recalculate all from scratch?
         term *= (a+k) * x / (b+k) / (k+1); //extending the pochhammer and factorial parts of the kth term. 
         result += term; //continue the sum
-        if(fabs(term) <= EPS * fabs(result)){ //i.e. is this change extremely small?
-            break;
-        }
+        k++;
     }
     return result;
 }
